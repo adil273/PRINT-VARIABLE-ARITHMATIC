a5c2b02b319db8d389cfc65c7edcfd7a5a6a27cb
Muhammad Adil <141252873+adil273@users.noreply.github.com>
2023-08-23 23:01:40 +0500

    Add files via upload

---
+++ VARIABLES.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+using namespace std;
+int main(){
+int age=25;
+cout<<age<<endl;
+
+age=19;
+cout<<age<<endl;
+
+int salary=1500;
+    cout<<salary<<endl;
+    salary=3000;
+    cout<<salary<<endl;
+
+
+
+    int number1 = 74;
+    cout << number1 << endl;
+
+    int number2 = 243;
+
+    // assign value stored in number2 to number1
+    number1 = number2;
+    cout << number1<<endl;
+
+
+    int distance = 134;
+    cout << distance << endl;
+
+    // create another variable distance2
+    int distance2 = 564;
+
+    // assign the value of distance2 to distance
+    distance = distance2;
+
+    // print distance
+    cout << distance;
+
+
+return 0;
+
+}
+
